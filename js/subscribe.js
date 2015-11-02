7347e97017dc3630f39ebfb1a77c20c9074c9298
Jie Liang <lj925184928@gmail.com>
2015-11-02 12:10:32 +0800

    Site updated: 2015-11-02 12:10:31

---
+++ js/subscribe.js
@@ -0,0 +1,46 @@
+$(function() {
+  var checker = localStorage.getItem("swiftweeklysubscribed")
+  if (checker) {
+    return
+  } else {
+    function checkemail(val) {
+      if (!val.trim()) {
+        return false
+      }
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+      return re.test(val)
+    }
+    $(document).scroll(function() {
+      checker = localStorage.getItem("swiftweeklysubscribed")
+      if (checker) {
+        return
+      }
+      var body = document.body
+      var html = document.documentElement
+      var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+      var y = $(this).scrollTop()
+      if (y > height * 0.618 ) {
+        $('#swiftweekly').fadeIn()
+      }
+    })
+    $(".sw_sub").click(function() {
+      $("#swiftweekly #content").submit()
+    })
+    $("#swiftweekly #content").submit(function(e) {
+      e.preventDefault()
+      if (!checkemail($("#sw_email").val())) {
+        setTimeout('$("#sw_email").focus()', 100)
+        return
+      }
+      $("#sw_email").addClass("disabled")
+      $(".sw_sub").addClass("disabled").text("稍等")
+      $.getJSON("http://ggchecker.githuber.info/addemail/" + $("#swiftweekly #sw_email").val().trim(), function(data) {
+        if (data["success"] == "success") {
+          localStorage.setItem("swiftweeklysubscribed", true)
+          $(".sw_sub").text("成功！")
+          setTimeout("$('#swiftweekly').fadeOut()", 1000);
+        }
+      })
+    })
+  }
+})
